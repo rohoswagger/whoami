15fa8073e23e059977e25dee8bfa1a1601e489ac
Roshan Desai <rohod04@gmail.com>
2025-01-20 23:55:25 -0800

    c

--- app/components/Writings.tsx
+++ app/components/Writings.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { getAllWritings, Writing } from "@/utils/writingsUtils";
+import { getAllWritings } from "@/utils/writingsUtils";
 
 const Writings: React.FC = async () => {
   const posts = await getAllWritings();
