c50c7284b9858f3aba1346f6e30a34bd028d1e27
rohoswagger <rohod04@gmail.com>
2025-09-10 20:56:02 -0700

    github link

--- public/data/links.ts
+++ public/data/links.ts
@@ -2,7 +2,7 @@ import { FaGithub, FaBook, FaSpotify } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 export const LINKS = [
-  { name: "Github", icon: FaGithub, href: "https://github.com/roshan-d" },
+  { name: "Github", icon: FaGithub, href: "https://github.com/rohoswagger" },
   {
     name: "X",
     icon: FaXTwitter,
